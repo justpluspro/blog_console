059c75782d4a7cea2666764d53467cc64bdd49bf
qwli7 <228794754@qq.com>
2021-03-22 00:01:57 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+// import store from './store'
+import axios from 'axios'
+import ElementUI from 'element-ui'
+
+Vue.use(axios)
+Vue.use(ElementUI)
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
-  store,
+  // store,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/views/Login.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <el-input v-model="input" placeholder="请输入用户名"></el-input>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login',
+  data () {
+    return {
+      input: 'ee'
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
