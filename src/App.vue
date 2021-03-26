ac91fa4b0859df018d624b49e8868faac3e8ff00
qwli7 <228794754@qq.com>
2021-03-26 23:50:18 +0800

    login page

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,15 @@
 <template>
   <div id="app">
-    <el-container>
-      <el-header>
-        <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" router @select="handleSelect">
-          <el-menu-item index="/console/articles">文章管理</el-menu-item>
-          <el-menu-item index="/console/moments">动态管理</el-menu-item>
-          <el-menu-item index="/console/files">文件管理</el-menu-item>
-          <el-menu-item index="/console/categories">分类管理</el-menu-item>
-          <el-menu-item index="/console/tags">标签管理</el-menu-item>
-          <el-menu-item index="/console/blackips">黑名单管理</el-menu-item>
-          <el-menu-item index="/console/configs">系统设置</el-menu-item>
-          <el-submenu index="2">
-            <template slot="title">我的工作台</template>
-            <el-menu-item index="2-1">选项1</el-menu-item>
-            <el-menu-item index="2-2">选项2</el-menu-item>
-            <el-menu-item index="2-3">选项3</el-menu-item>
-            <el-submenu index="2-4">
-              <template slot="title">选项4</template>
-              <el-menu-item index="2-4-1">选项1</el-menu-item>
-              <el-menu-item index="2-4-2">选项2</el-menu-item>
-              <el-menu-item index="2-4-3">选项3</el-menu-item>
-            </el-submenu>
-          </el-submenu>
-          <el-menu-item index="3" disabled>消息中心</el-menu-item>
-          <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
-        </el-menu>
-        <div class="line"></div>
-      </el-header>
-      <el-main>
-        <router-view/>
-      </el-main>
-    </el-container>
+    <router-view />
   </div>
 </template>
 <script>
-import Vue from 'vue'
-import ElementUI from 'element-ui'
-Vue.use(ElementUI)
+
 export default {
   data () {
-    return {
-      activeIndex: '1',
-      activeIndex2: '1'
-    }
+    return {}
   },
   methods: {
-    handleSelect () {
-      console.log('选中了')
-    }
   }
 }
 </script>
@@ -55,11 +17,6 @@ export default {
 <style lang="scss">
 
 * {
-  padding: 0;
-  margin: 0;
-}
-
-#app {
   padding: 0;
   margin: 0;
 }
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,20 +1,86 @@
 <template>
-  <div>
-    <el-input v-model="input" placeholder="请输入用户名"></el-input>
+  <div class="container">
+    <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" class="demo-ruleForm">
+      <div class="login-title">用户登录</div>
+      <el-form-item label="" prop="username">
+        <el-input type="password" v-model="ruleForm.username" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="" prop="password">
+        <el-input type="password" v-model="ruleForm.password" autocomplete="off"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" :loading="loading" @click="submitForm('ruleForm')">提交</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'login',
   data () {
+    const validateName = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入用户名'))
+      } else {
+        if (this.ruleForm.username !== '') {
+          this.$ref.ruleForm.validateField('username')
+        }
+        callback()
+      }
+    }
+    const validatePass = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入密码'))
+      } else {
+        if (this.ruleForm.password !== '') {
+          this.$ref.ruleForm.validateField('password')
+        }
+        callback()
+      }
+    }
     return {
-      input: 'ee'
+      loading: false,
+      ruleForm: {
+        username: '',
+        password: ''
+      },
+      rules: {
+        username: [
+          { validator: validateName, trigger: 'blur' }
+        ],
+        password: [
+          { validator: validatePass, trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    submitForm () {
+      console.log('登录')
+      this.loading = !this.loading
+      // this.$store.dispatch()
     }
   }
 }
 </script>
 
 <style scoped lang="scss">
-
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background: #2C2319;
+  .login-title {
+    text-align: center;
+    font-size: 2rem;
+    color: #ffffff;
+    margin-bottom: 16px;
+  }
+}
+.el-button {
+  width: 100%;
+}
 </style>
